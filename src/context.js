6924ef9c67b904fe2e13468223ddd5e0f5e1a643
jai-sharma <sendtojsharma@gmail.com>
2021-05-18 11:22:58 +0530

    adding files

---
+++ src/context.js
@@ -0,0 +1,85 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { getHashedString } from './lib/getHashedString';
+import { getTranslatedText } from './lib/getTranslatedText';
+
+const TranslateContext = createContext();
+
+let originalArrayCopy = []; // storing default locale textArray
+function TranslateProvider({ defaultLocale = 'en', children }) {
+    const [locale, setLocale] = useState(defaultLocale);
+    let [textArray, setTextArray] = useState([]);
+
+    const t = (str) => {
+        const id = getHashedString(str);
+
+        let hasId = false;
+        textArray.forEach((textObj) => {
+            if (textObj.id === id) hasId = true;
+        });
+
+        if (!hasId) {
+            textArray.push({ id: id, str: str });
+        }
+
+        let text = '';
+        textArray.forEach((textObj) => {
+            if (textObj.id === id) text = textObj.str;
+        });
+
+        return text;
+    };
+
+    useEffect(() => {
+        // locale is default
+        if (locale === defaultLocale) {
+            if (originalArrayCopy.length === 0) {
+                originalArrayCopy = JSON.parse(JSON.stringify(textArray));
+                return;
+            } else {
+                setTextArray(originalArrayCopy);
+                return;
+            }
+        }
+
+        // locale is not default
+        let AllText = '';
+        originalArrayCopy.map((textObj) => {
+            AllText += `${textObj.str}\n`;
+        });
+
+        getTranslatedText(AllText, locale).then((translatedText) => {
+            const translatedtTextArray = translatedText.split('\n');
+
+            const newTextArray = JSON.parse(JSON.stringify(textArray));
+            translatedtTextArray.map((eachLine, idx) => {
+                newTextArray[idx].str = eachLine;
+            });
+            setTextArray(newTextArray);
+        });
+    }, [locale]);
+
+    return (
+        <TranslateContext.Provider value={{ locale, setLocale, t }}>
+            {children}
+        </TranslateContext.Provider>
+    );
+}
+
+const useLocale = () => {
+    const { locale, setLocale } = useContext(TranslateContext);
+
+    return {
+        locale,
+        setLocale,
+    };
+};
+
+const useTranslation = () => {
+    const { t } = useContext(TranslateContext);
+
+    return {
+        t,
+    };
+};
+
+export { TranslateProvider, useLocale, useTranslation };
---
+++ src/lib/getTranslatedText.js
@@ -0,0 +1,18 @@
+const getTranslatedText = async (str, locale) => {
+    const postData = {
+        text: str,
+        toTranslate: locale,
+    };
+
+    const data = await fetch('/api/translate', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json;charset=UTF-8' },
+        body: JSON.stringify(postData),
+    });
+
+    const text = await data.json();
+    const translatedText = await text.translatedText;
+    return translatedText;
+};
+
+export { getTranslatedText };
