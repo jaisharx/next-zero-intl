999fd6cb3c08dcec8c0781f4da0141a5af5b5894
jai-sharma <sendtojsharma@gmail.com>
2021-05-18 11:39:26 +0530

    adding api

---
+++ api/src/index.js
@@ -0,0 +1,19 @@
+import translate from '@vitalets/google-translate-api';
+
+const handleTranslation = async (req, res) => {
+    if (req.method === 'POST') {
+        // Process a POST request
+        const text = req.body.text;
+        const toTranslate = req.body.toTranslate;
+
+        const data = await translate(text, { to: toTranslate });
+        const translatedText = await data.text;
+
+        res.status(200).json({ translatedText });
+    } else {
+        // Handle any other HTTP method
+        res.status(200).json({ error: 'this route is POST only' });
+    }
+};
+
+export { handleTranslation };
