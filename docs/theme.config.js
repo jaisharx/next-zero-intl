46265aafeae98225257a85d023a0be42859a849b
jai-sharma <sendtojsharma@gmail.com>
2021-05-19 19:36:59 +0530

    updating docs

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,16 +1,16 @@
 export default {
-    repository: 'https://github.com/shuding/nextra', // project repo
-    docsRepository: 'https://github.com/shuding/nextra', // docs repo
-    branch: 'master', // branch of docs
+    repository: 'https://github.com/jaisharx/next-zero-intl', // project repo
+    docsRepository: 'https://github.com/jaisharx/next-zero-intl/tree/main/docs', // docs repo
+    branch: 'main', // branch of docs
     path: '/', // path of docs
-    titleSuffix: ' – Nextra',
+    titleSuffix: ' – Zero Config Internationalization',
     nextLinks: true,
     prevLinks: true,
     search: true,
     customSearch: null, // customizable, you can use algolia for example
     darkMode: true,
     footer: true,
-    footerText: 'MIT 2020 © Shu Ding.',
+    footerText: 'MIT 2021 © Jai Sharma',
     footerEditOnGitHubLink: true, // will link to the docs repo
     logo: (
         <>
@@ -24,8 +24,8 @@ export default {
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
             />
-            <meta name="description" content="Nextra: the next docs builder" />
-            <meta name="og:title" content="Nextra: the next docs builder" />
+            <meta name="description" content="Zero hassle internationalization for your next.js apps." />
+            <meta name="og:title" content="Next Zero Intl – Zero Config Internationalization" />
         </>
     ),
 };
