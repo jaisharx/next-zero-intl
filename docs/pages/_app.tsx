53b29f72c0c3313dbef08f8a32e51f4e67d52236
jai-sharma <sendtojsharma@gmail.com>
2021-08-06 19:56:57 +0530

    migrating docs to typescript

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from 'next/app'
 import 'nextra-theme-docs/style.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
