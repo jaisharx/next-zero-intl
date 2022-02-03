f282810def7a2216d3d836899bb67e12fdd6651d
Jai Sharma <sendtojsharma@gmail.com>
2022-02-03 11:39:14 +0530

    full package refactor => moving to turbo-repo

---
+++ apps/demo/next.config.js
@@ -0,0 +1,5 @@
+const withTM = require('next-transpile-modules')(['next-zero-intl'])
+
+module.exports = withTM({
+  reactStrictMode: true,
+})
---
+++ packages/next-zero-intl/context.tsx
@@ -0,0 +1,86 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { getHashedString } from './lib/getHashedString'
+import { getTranslatedText } from './lib/getTranslatedText'
+
+const TranslationContext = createContext({
+  locale: undefined,
+  setLocale: undefined,
+  t: undefined,
+})
+
+let originalArrayCopy = [] // storing default locale textArray
+function TranslationProvider({ defaultLocale = 'en', children }) {
+  const [locale, setLocale] = useState(defaultLocale)
+  let [textArray, setTextArray] = useState([])
+
+  const t = (str: string) => {
+    const id = getHashedString(str)
+
+    let hasId = false
+    textArray.forEach((textObj) => {
+      if (textObj.id === id) hasId = true
+    })
+
+    if (!hasId) {
+      textArray.push({ id: id, str: str })
+    }
+
+    let text = ''
+    textArray.forEach((textObj) => {
+      if (textObj.id === id) text = textObj.str
+    })
+
+    return text
+  }
+
+  useEffect(() => {
+    // locale is default
+    if (locale === defaultLocale) {
+      if (originalArrayCopy.length === 0) {
+        originalArrayCopy = JSON.parse(JSON.stringify(textArray))
+        return
+      } else {
+        setTextArray(originalArrayCopy)
+        return
+      }
+    }
+
+    // locale is not default
+    let AllText = ''
+    originalArrayCopy.map((textObj) => {
+      AllText += `${textObj.str}\n`
+    })
+
+    getTranslatedText(AllText, locale).then((translatedText) => {
+      const translatedtTextArray = translatedText.split('\n')
+
+      const newTextArray = JSON.parse(JSON.stringify(textArray))
+      translatedtTextArray.map((eachLine, idx) => {
+        newTextArray[idx].str = eachLine
+      })
+      setTextArray(newTextArray)
+    })
+  }, [locale])
+
+  return (
+    <TranslationContext.Provider value={{ locale, setLocale, t }}>
+      {children}
+    </TranslationContext.Provider>
+  )
+}
+
+const useLocale = () => {
+  const { locale, setLocale } = useContext(TranslationContext)
+
+  return [locale, setLocale]
+}
+
+const useTranslation = () => {
+  const { t } = useContext(TranslationContext)
+
+  return {
+    t,
+  }
+}
+
+export { TranslationProvider, useLocale, useTranslation }
