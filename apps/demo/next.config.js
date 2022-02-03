4641c9bc68d2ee0a48f7e9e88f026055e4db2314
Jai Sharma <sendtojsharma@gmail.com>
2022-02-03 11:46:17 +0530

    updating next config

--- apps/demo/next.config.js
+++ apps/demo/next.config.js
@@ -1,5 +1,15 @@
 const withTM = require('next-transpile-modules')(['next-zero-intl'])
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = withTM({
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
 })
